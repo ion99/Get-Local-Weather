b1aa6cc5fa1ea263dfc6da262e760b513edb719c
Ion Gorincioi <gorincioii@yahoo.com>
2016-07-06 23:32:23 -0700

    getting exact weather determining geolocation first

---
+++ weatherGeoLoc.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  function getLocalWeather(){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position){
+        
+      $.getJSON("https://api.wunderground.com/api/c451ff398a91d3b1/geolookup/q/" + position.coords.latitude + "," + position.coords.longitude + ".json", function(data) {     
+        $.getJSON("https://api.wunderground.com/api/c451ff398a91d3b1/conditions/q/" + data.location.nearby_weather_stations.airport.station[0].state + "/" + data.location.city + ".json", function(data1) {
+          let html = "";
+          let html1 = "";
+
+          html += "<div class='weather'>" + data1.current_observation.display_location.full + "<br>" + "<div class='far'>" + data1.current_observation.temp_f + " " + "<span class='cf clickable'>&#8457;</span>" + "</div>" +
+  data1.current_observation.weather + "<br>" + "<img src='" + data1.current_observation.icon_url.slice(0,4) + "s" + data1.current_observation.icon_url.slice(4) + "'" + "height='150' width='150'>" + "</div>";
+          
+          html1 += "<div class='weather'>" + data1.current_observation.display_location.full + "<br>" + "<div class='cel'>" + data1.current_observation.temp_c + " " + "<span class='fc clickable'>&#8451;</span>" + "</div>" +
+  data1.current_observation.weather + "<br>" + "<img src='" + data1.current_observation.icon_url.slice(0,4) + "s" + data1.current_observation.icon_url.slice(4) + "'" + "height='150' width='150'>" + "</div>";
+            
+          $("#data").html(html);
+          
+          $(".cf").click(function(){
+            $("#data").html(html1);
+            $(".fc").click(function(){
+              $("#data").html(html);
+              getLocalWeather();
+            })
+          })
+        });
+      });
+     });
+   }
+  } 
+  getLocalWeather();      
+})  
